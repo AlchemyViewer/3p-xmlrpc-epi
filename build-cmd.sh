7e89a72a4acb4b11b5be1a129975deef9d8fc330
Rye Mutt <rye@alchemyviewer.org>
2021-12-14 20:17:44 -0500

    Bump macOS ABI to 10.15

--- build-cmd.sh
+++ build-cmd.sh
@@ -69,7 +69,7 @@ pushd "$XMLRPCEPI_SOURCE_DIR"
             # Setup osx sdk platform
             SDKNAME="macosx"
             export SDKROOT=$(xcodebuild -version -sdk ${SDKNAME} Path)
-            export MACOSX_DEPLOYMENT_TARGET=10.13
+            export MACOSX_DEPLOYMENT_TARGET=10.15
 
             # Setup build flags
             ARCH_FLAGS="-arch x86_64"
